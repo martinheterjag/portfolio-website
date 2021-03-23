66318bbf32d00ea1149eecb69f0c6afbf7353f57
martinheterjag <martine7@kth.se>
2021-03-24 00:15:02 +0100

    Split Eurorack modules to its own project page

--- src/11ty/_data/portfolioList.js
+++ src/11ty/_data/portfolioList.js
@@ -26,81 +26,6 @@ module.exports = [
 		],
 		moreDetails: [
 			{
-				sectionHeader: "Eurorack wooden case and PSU",
-				sectionHeaderImageName: "case",
-				sectionDetails: `The case is made in pinewood and painted, four 84 HP Eurorack rails are mounted in the case, which makes it a 6U 84hp rack.
-					The PSU PCB have a +- 12V rails as well as a 5V rail for digital modules. The PSU schematic and PCB is designed in KiCad.`,
-			},
-			{
-				sectionHeader: "",
-				sectionHeaderImageName: "wip-case",
-				sectionDetails: `A picture of the case before it was painted.`,
-			},
-			{
-				sectionHeader: "",
-				sectionHeaderImageName: "3d-view",
-				sectionDetails: `A 3D model of the PSU.`,
-			},
-			{
-				sectionHeader: "",
-				sectionHeaderImageName: "powerbus",
-				sectionDetails: `Eurorack bus to connect modules to the PSU.`,
-			},
-			{
-				sectionHeader: "Eurorack module LPG",
-				sectionHeaderImageName: "lpg",
-				sectionDetails: `A classic module, the Buchla 292. Later it was cloned by Peter Grenader and released as a part of on Ken Stone's
-					CGS project. I based it on the schematics found on Ken Stone's website but tweaked it a little bit and made a new PCB design in KiCad.
-					The faceplate is made in aluminium, ahnd drilled and hand painted`,
-			},
-			{
-				sectionHeader: "Eurorack module CEM3340 VCO",
-				sectionHeaderImageName: "cem3340",
-				sectionDetails: `Dual VCO based on the classic CEM3340 IC used in vintage synths like Roland SH-101, Moog Memorymoog,
-					Sequental Circuits Prophet 5 and many more. Each VCO has saw and triangle outputs and 1V/oct and frequency CV inputs.
-					There is a switch for turning on soft sync or hard sync between the two oscillators. If you feed the VCO with a negative voltage
-					it is possible to use it as an LFO. The circuit and PCB was designed in KiCad, the faceplate is painted aluminium.`,
-			},
-			{
-				sectionHeader: "Eurorack module Drum Machine",
-				sectionHeaderImageName: "drum-machine",
-				sectionDetails: `A digital drum module with analog interface, based on Raspbery Pi Zero. You can find the project on this
-					<a class='text-custom-blue' href=https://github.com/martinheterjag/eurorack-drum-machine>github repository</a>. The circuit is
-					soldered on a perf board with headers to mount the Raspbery Pi Zero. There are interfaces tp trigger a bd, sn, cp, ch, oh
-					and cy drum sounds. The actual sounds can be replaced by logging in to the Raspberry Pi Zero via SSH and replace the files.
-					I am using the JUCE C++ framework to trigger the sounds, and i cross compile the project using a gcc tool-chain for raspberry pi.
-					For some reason i am having latency issues when the sounds are played, I've tried with different programming languages and
-					libraries and i found JUCE having the lowest latency, but it's still pretty bad. It is on my to-do list to fix this issue.<br><br>
-					Also, i drew R2D2 on the faceplate, because robots are cool!`,
-			},
-			{
-				sectionHeader: "Eurorack module mixer modules",
-				sectionHeaderImageName: "mixer",
-				sectionDetails: `Two different mixer modules, one with knob for each input and input 1 is normalized to 12V to act as an offset knob if needed.
-					The other one have a smaller footprint and a higher gain to boost signals if needed. Both mixers are based on the same circuit, an opamp
-					coupled as a summing inverting amplifier followed by an inverter. The larger mixer also have an output before the second stage to get an
-					inverted output`,
-			},
-			{
-				sectionHeader: "",
-				sectionHeaderImageName: "mix",
-				sectionDetails: `Picture of the mixer with more gain and smaller footprint.`,
-			},
-			{
-				sectionHeader: "Skarp Plingeling",
-				sectionHeaderImageName: "pling-vco",
-				sectionDetails: `Skarp Plingeling was a competence development project I did together with a collegue, Linus Fredriksson, at Skarp
-				(Note, when this project was made the company <i>Skarp</i> was named <i>Drakryggen</i>. The name change was done in march 2021). We made a VCA module, a VCO and started to design a state veriable filter.
-				Unforunately (very very sad) Linus passed away durign the project due to sudden health issues, and it was never finished.
-				Both the VCA and VCO was based of the LM13700 IC (a dual transconductance amplifier). The VCA and VCO was was assemblied but no enclosure was built.
-				Simulation was done in LTspice, schematics and PCB design in Eagle and KiCad`,
-			},
-			{
-				sectionHeader: "",
-				sectionHeaderImageName: "pling-vca",
-				sectionDetails: `Picture of the VCA circuit.`,
-			},
-			{
 				sectionHeader: "CV Gate Keyboard",
 				sectionHeaderImageName: "red-keyboard",
 				sectionDetails: `It's made out of wood, it's red and it's beautiful. It contains a +- 12V PSU that i soldered to a perf board and a
@@ -123,6 +48,20 @@ module.exports = [
 				sectionDetails: `Numbering every key to make it easier to assemble them in correct order.`,
 			},
 			{
+				sectionHeader: "Skarp Plingeling",
+				sectionHeaderImageName: "pling-vco",
+				sectionDetails: `Skarp Plingeling was a competence development project I did together with a collegue, Linus Fredriksson, at Skarp
+				(Note, when this project was made the company <i>Skarp</i> was named <i>Drakryggen</i>. The name change was done in march 2021). We made a VCA module, a VCO and started to design a state veriable filter.
+				Unforunately (very very sad) Linus passed away durign the project due to sudden health issues, and it was never finished.
+				Both the VCA and VCO was based of the LM13700 IC (a dual transconductance amplifier). The VCA and VCO was was assemblied but no enclosure was built.
+				Simulation was done in LTspice, schematics and PCB design in Eagle and KiCad`,
+			},
+			{
+				sectionHeader: "",
+				sectionHeaderImageName: "pling-vca",
+				sectionDetails: `Picture of the VCA circuit.`,
+			},
+			{
 				sectionHeader: "\"Gula Guran\", the yellow electric guitar",
 				sectionHeaderImageName: "gula-guran",
 				sectionDetails: `I assemblied and painted this guitar from a harley benton kit. I don't know what makes a good guitar, but it works and it's
@@ -138,6 +77,92 @@ module.exports = [
 					this way I can control the sequence length from 1 to 10 steps. The IC have a pin for cartry output, this pin is high on step 1 through 5
 					and low on step 6 through 10, i have made this pin available as an output as well for some extra gate action, I use it about once every
 					4 years, so you can probably live without it if you want to build something similar.`,
+			},
+		]
+	},
+	{
+		id: "eurorack",
+		title: "Eurorack synth modules",
+		description:
+			"Hardware synthesizers, in Eurorack format.",
+		imagePath: "/assets/images/eurorack",
+		heroName: "hero",
+		url: "/project/eurorack",
+		bgColor: "bg-pink-200",
+		role: [
+			"Electronics engineering, I've been designing and building these projects.",
+		],
+		tech: [
+			"Electronics",
+			"Circuit design",
+			"KiCad",
+			"JUCE",
+			"Raspberry Pi"
+		],
+		about: [
+			`...`,  // TODO: Describe Eurorack and this project.
+		],
+		moreDetails: [
+			{
+				sectionHeader: "Wooden case and PSU",
+				sectionHeaderImageName: "case",
+				sectionDetails: `The case is made in pinewood and painted, four 84 HP Eurorack rails are mounted in the case, which makes it a 6U 84hp rack.
+					The PSU PCB have a +- 12V rails as well as a 5V rail for digital modules. The PSU schematic and PCB is designed in KiCad.`,
+			},
+			{
+				sectionHeader: "",
+				sectionHeaderImageName: "wip-case",
+				sectionDetails: `A picture of the case before it was painted.`,
+			},
+			{
+				sectionHeader: "",
+				sectionHeaderImageName: "3d-view",
+				sectionDetails: `A 3D model of the PSU.`,
+			},
+			{
+				sectionHeader: "",
+				sectionHeaderImageName: "powerbus",
+				sectionDetails: `Eurorack bus to connect modules to the PSU.`,
+			},
+			{
+				sectionHeader: "Low Pass Gate",
+				sectionHeaderImageName: "lpg",
+				sectionDetails: `A classic module, the Buchla 292. Later it was cloned by Peter Grenader and released as a part of on Ken Stone's
+					CGS project. I based it on the schematics found on Ken Stone's website but tweaked it a little bit and made a new PCB design in KiCad.
+					The faceplate is made in aluminium, ahnd drilled and hand painted`,
+			},
+			{
+				sectionHeader: "CEM3340 VCO",
+				sectionHeaderImageName: "cem3340",
+				sectionDetails: `Dual VCO based on the classic CEM3340 IC used in vintage synths like Roland SH-101, Moog Memorymoog,
+					Sequental Circuits Prophet 5 and many more. Each VCO has saw and triangle outputs and 1V/oct and frequency CV inputs.
+					There is a switch for turning on soft sync or hard sync between the two oscillators. If you feed the VCO with a negative voltage
+					it is possible to use it as an LFO. The circuit and PCB was designed in KiCad, the faceplate is painted aluminium.`,
+			},
+			{
+				sectionHeader: "Drum Machine",
+				sectionHeaderImageName: "drum-machine",
+				sectionDetails: `A digital drum module with analog interface, based on Raspbery Pi Zero. You can find the project on this
+					<a class='text-custom-blue' href=https://github.com/martinheterjag/eurorack-drum-machine>github repository</a>. The circuit is
+					soldered on a perf board with headers to mount the Raspbery Pi Zero. There are interfaces tp trigger a bd, sn, cp, ch, oh
+					and cy drum sounds. The actual sounds can be replaced by logging in to the Raspberry Pi Zero via SSH and replace the files.
+					I am using the JUCE C++ framework to trigger the sounds, and i cross compile the project using a gcc tool-chain for raspberry pi.
+					For some reason i am having latency issues when the sounds are played, I've tried with different programming languages and
+					libraries and i found JUCE having the lowest latency, but it's still pretty bad. It is on my to-do list to fix this issue.<br><br>
+					Also, i drew R2D2 on the faceplate, because robots are cool!`,
+			},
+			{
+				sectionHeader: "Mixer modules",
+				sectionHeaderImageName: "mixer",
+				sectionDetails: `Two different mixer modules, one with knob for each input and input 1 is normalized to 12V to act as an offset knob if needed.
+					The other one have a smaller footprint and a higher gain to boost signals if needed. Both mixers are based on the same circuit, an opamp
+					coupled as a summing inverting amplifier followed by an inverter. The larger mixer also have an output before the second stage to get an
+					inverted output`,
+			},
+			{
+				sectionHeader: "",
+				sectionHeaderImageName: "mix",
+				sectionDetails: `Picture of the other mixer module with more gain and smaller footprint.`,
 			},
 		]
 	},
